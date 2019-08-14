4ba8d3ab38c31cd13e9693f92352f2ecf2010a29
Adrianbaldonado <baldonado.adrian@yahoo.com>
2019-08-14 14:34:49 -0700

    Added exercise 11

---
+++ exercises/exercise_11.py
@@ -0,0 +1,14 @@
+"""Asking Qestions
+
+
+The purpose of this exercise is to utilize all ive learned so far
+"""
+
+print("How old are you?", end=' ')
+age = input(22)
+print("How tall are you?", end=' ')
+height = input(5)
+print("How much do you weigh?", end=' ')
+weight = input(160)
+
+print(f"So, you\'re{age} old, {height} tall and {weight} heavy.")
