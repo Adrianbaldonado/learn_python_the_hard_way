bccefe62b99335095ac956281393cc424c9654b6
Adrianbaldonado <baldonado.adrian@yahoo.com>
2019-08-15 14:17:53 -0700

    Added exercise 12

---
+++ exercises/exercise_12.py
@@ -0,0 +1,10 @@
+"""Prompting people
+
+
+The purpose of this exercise is understanding how to input in python
+"""
+
+age = input("How old are you? ")
+height = input("How tall are? ")
+weight = input("How much do you weigh? ")
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
